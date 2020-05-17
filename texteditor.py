650aa792de56eb232dc4073cba1fc533da296caf
heliorrfreitas <heliorui.ramosfreitas@gus.pittstate.edu>
2020-05-17 00:06:06 -0500

    I edited the code a little bit, a class was created and shortcuts were added to allow the user to control the edition of the text using commands like Ctrl+S to save for example.

--- texteditor.py
+++ texteditor.py
@@ -1,59 +1,98 @@
-from tkinter import *
+import tkinter as tk
 from tkinter.filedialog import *
+import sys
 
-filename = None
-def new_file():
-    global filename
-    filename = "Untitled"
-    text.delete(0.0, END)
+class App(tk.Tk):
 
-def save_file():
-    global filename
-    t = text.get(0.0, END)
-    f = open(filename, 'w')
-    f.write(t)
-    f.close()
+    def quit(self, event):
+        print("quitting...")
+        sys.exit(0)
 
-def save_file_as():
-    f = asksaveasfile(mode='w', defaultextension=".txt")
-    t = text.get(0.0, END)
+    def new(self, event): 
+        print("new file ...")       
+        self.filename = "Untitled"
+        self.text.delete(0.0, tk.END)    
     
-    try:
-        f.write(t.rstrip())
-    except:
-        showerror(title="Error", message="Unable to save file.")
+    def save(self, event):
+        print('update file...')
+        t = self.text.get(0.0, tk.END)
+        f = open(self.filename, 'w')
+        f.write(t)
+        f.close()
+    
+    def saveas(self, event):
+        print("Save file as...")
+        f = tk.filedialog.asksaveasfile(mode='w', defaultextension=".txt")
+        t = self.text.get(0.0, tk.END)
+        
+        try:
+            f.write(t.rstrip())
+        except:
+            self.showerror(title="Error", message="Unable to save file.")
 
-    global filename
-    filename = f.name
-
-def open_file():
-    f = askopenfile(mode='r')
-    t = f.read()
-    text.delete(0.0, END)
-    text.insert(0.0, t)
-    global filename
-    filename = f.name
-
-root = Tk()
-root.title("Text Editor")
-root.minsize(width=400, height=400)
-root.maxsize(width=400, height=400)
-
-text = Text(root, width=400, height=400)
-text.pack()
-
-menubar = Menu(root)
-filemenu = Menu(menubar)
-filemenu.add_command(label="New", command=new_file)
-filemenu.add_command(label="Open", command=open_file)
-filemenu.add_command(label="Save", accelerator="Ctrl+S", command=save_file)
-filemenu.add_command(label="Save As...", command=save_file_as)
-filemenu.add_separator()
-filemenu.add_command(label="Quit", command=root.quit)
-menubar.add_cascade(label="File", menu=filemenu)
-
-root.config(menu=menubar)
-root.mainloop()
+        self.filename = f.name
+    
+    def open(self, event):
+        print("Open file...")
+        f = tk.filedialog.askopenfile(mode='r')
+        t = f.read()
+        self.text.delete(0.0, tk.END)
+        self.text.insert(0.0, t)
+        self.filename = f.name
 
 
-# Tutorial from https://www.youtube.com/watch?v=xqDonHEYPgA
+    def menuoptions(self, event):
+        print("AQUI")
+
+    def __init__(self):
+        tk.Tk.__init__(self)
+
+        self.title("My First Text Editor")
+        self.minsize(width=400, height=400)
+        self.maxsize(width=400, height=400)
+        
+        menubar = tk.Menu(self)
+        
+        filemenu = tk.Menu(menubar, tearoff=False)
+        
+        menubar.add_cascade(label="File", underline=0, menu=filemenu)
+        
+
+        filemenu.add_command(label="New", underline=1,
+                                command=quit, accelerator="Ctrl+N")
+
+        filemenu.add_command(label="Open", underline=2,
+                                command=open, accelerator="Ctrl+O")
+
+        filemenu.add_command(label="Save", underline=3,
+                                command=quit, accelerator="Ctrl+S")
+
+
+        filemenu.add_command(label="Save As...", underline=4,
+                                command=quit, accelerator="Ctrl+Shift+S")
+
+        
+        filemenu.add_command(label="Exit", underline=5,
+                                command=quit, accelerator="Ctrl+Q")
+
+
+
+        self.config(menu=menubar)
+        self.bind_all("<Control-n>", self.new)
+        self.bind_all("<Control-q>", self.quit)
+        self.bind_all("<Control-s>", self.save)
+        self.bind_all("<Control-Shift-S>", self.saveas)
+        self.bind_all("<Control-o>", self.open)
+        
+        self.filename = None
+        
+        self.text = Text(self, width=400, height=400)
+        self.text.pack()
+
+    
+
+    
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
